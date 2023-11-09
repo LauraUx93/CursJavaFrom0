c6a80589cbf78757c76be7eba6a75c04d4714983
lauraungurianu <145116556+lauraungurianu@users.noreply.github.com>
2023-11-09 13:38:00 +0200

    curs0

---
+++ curs0/curs0.java
@@ -0,0 +1,20 @@
+package curs0;
+
+public class curs0 {
+    public static void main(String[] args) {
+        System.out.println("curs1");
+        System.out.println("curs1new");
+        System.out.print("curs10");
+//        System.out.print("curs10");
+        int vitezaMax = 230;
+        float costMasina = 123.56F;
+        String nota = "A";
+        String marcaMasina = "Logan";
+        boolean esteInmatriculata = true;
+        double sumaDouble = vitezaMax + costMasina;
+        System.out.println(vitezaMax);
+
+        System.out.println(marcaMasina + vitezaMax);
+        System.out.println(sumaDouble);
+    }
+}
---
+++ curs1.java
@@ -0,0 +1,2 @@
+public class curs1 {
+}
