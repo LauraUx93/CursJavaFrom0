cbbd4e67a42c9028f87b6a112517889c353d51bd
lauraungurianu <145116556+lauraungurianu@users.noreply.github.com>
2023-11-09 13:54:42 +0200

    Curs0 update
    operatori, variabile

--- curs0/curs0.java
+++ curs0/curs0.java
@@ -7,14 +7,41 @@ public class curs0 {
         System.out.print("curs10");
 //        System.out.print("curs10");
         int vitezaMax = 230;
-        float costMasina = 123.56F;
+        final double costMasina = 9.9; // final inseamna ca nu mai putem modifica variabila
         String nota = "A";
         String marcaMasina = "Logan";
         boolean esteInmatriculata = true;
         double sumaDouble = vitezaMax + costMasina;
         System.out.println(vitezaMax);
 
-        System.out.println(marcaMasina + vitezaMax);
-        System.out.println(sumaDouble);
+        System.out.println("Marca masina si viteza Max: " + marcaMasina + vitezaMax);
+        System.out.println("Suma double: " + sumaDouble);
+        int x,y,z;
+        x=y=z=60;
+        int a = 5, b = 6, c = 7;
+        System.out.println(("Viteza before: " + vitezaMax));
+        vitezaMax = 300;
+        System.out.println("Viteza after: " + vitezaMax);
+//        costMasina = 10; cannot make the change because costMasina is final
+
+
+        //operatori aritmetici
+        // =, -, /, *, %, ++, --
+        int rest = 6%5;
+        rest += rest;
+        rest++;
+        rest--;
+        System.out.println("Rest: " + rest);
+
+        //operatori de atribuire
+        //+=, -=, *=, /=
+        rest += 5;
+        System.out.println("Rest: " + rest);
+
+        //operatori de comparare
+        // ==, !=, >, <, <=, >=
+
+        //operatori logici
+        // &&, ||, !
     }
 }
